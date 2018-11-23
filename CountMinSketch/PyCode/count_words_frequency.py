301233cfbaa1bda37aea76488bf2e1d78524e0a2
panxin <xin-pan@outlook.com>
2018-11-23 16:42:02 +0800

    pythonCount

--- CountMinSketch/PyCode/count_words_frequency.py
+++ CountMinSketch/PyCode/count_words_frequency.py
@@ -1,8 +1,19 @@
 
+frequency_dict = dict()
 
-frequency_dict = {}
+word_file = open('words.txt', 'r')
 
-for line in open("dataset1words.txt"):
+for line in word_file:
+	line = line.strip('\n')
 	cur = line.split(',')
-	if frequency_dict.has_key(cur[0])
-	
+	if frequency_dict.has_key(cur[0]):
+		frequency_dict[cur[0]] = frequency_dict[cur[0]] + int(cur[1])
+	else:
+		frequency_dict[cur[0]] = int(cur[1])
+
+word_file.close()
+
+frequency_dict = sorted(frequency_dict.items(), key = lambda d: d[1], reverse = True)
+
+for i in range(10):
+	print(frequency_dict[i])
